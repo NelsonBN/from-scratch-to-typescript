b629ccb71f1f796422465d5163934e3729911915
Nelson Nobre <technobre@gmail.com>
2023-12-04 16:12:39 +0000

    Demo void type

---
+++ 03-Fundamentals/07-Void/app.ts
@@ -0,0 +1,7 @@
+function printValues(a: number, b: number): void {
+    console.log(`Values are ${a} and ${b}`);
+}
+printValues(2, 3);
+
+let Value: void = undefined;
+console.log(`Value is ${Value} with type ${typeof Value}`);
---
+++ 03-Fundamentals/08-Functions/app.ts
@@ -0,0 +1,18 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+console.log(sum(2, 3));
+
+function subtract(a: number, b: number)
+{
+    return a - b;
+}
+console.log(subtract(2, 3));
+
+const multiply = (a: number, b: number): number => {
+    return a * b;
+}
+console.log(multiply(2, 3));
+
+const divide = (a: number, b: number) => a / b;
+console.log(divide(2, 3));
