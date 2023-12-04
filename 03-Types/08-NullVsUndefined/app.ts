2bb7cd55c9a9e0e77a2e2d4c27aab331984505cf
Nelson Nobre <technobre@gmail.com>
2023-12-04 16:26:54 +0000

    Demo undefined vs null

---
+++ 03-Types/08-NullVsUndefined/app.ts
@@ -0,0 +1,20 @@
+let value1 = null;
+
+console.log(value1);
+console.log(typeof value1);
+
+
+console.log('------------------');
+
+
+let value2 = undefined;
+
+console.log(value2);
+console.log(typeof value2);
+
+
+console.log('------------------');
+
+
+console.log(null == undefined);
+console.log(null === undefined);
