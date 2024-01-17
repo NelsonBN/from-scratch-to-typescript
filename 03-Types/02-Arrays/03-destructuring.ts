811a0a3e916d58d665efe71d48b871e2801dc586
Nelson Nobre <technobre@gmail.com>
2024-01-17 16:55:18 +0000

    Array desctructuring examples

---
+++ 03-Types/02-Arrays/03-destructuring.ts
@@ -0,0 +1,14 @@
+const arr11: number[] = [ 9, 8, 7, 6, 5, 4, 3, 2, 1 ];
+
+const arr12 = [...arr11];
+const arr13 = [arr11];
+console.log(arr12);
+console.log(arr13);
+
+
+const arr21: number[][] = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
+
+const arr22 = [...arr21];
+const arr23 = [arr21];
+console.log(arr22);
+console.log(arr23);
